d62f36d2f365c2412cbefee7749f04acd3d4c727
sm-nastya2505@yandex.ru <sm.nastya2505@gmail.com>
2024-06-09 20:50:24 +0300

    test code added

---
+++ test.py
@@ -0,0 +1,26 @@
+import pytest
+from unittest.mock import patch
+from io import StringIO
+
+def print_hello_world_multiple_times():
+    num = int(input("Введите число: "))
+    for _ in range(num):
+        print("Hello, world")
+
+def test_print_hello_world_multiple_times_0(capsys):
+    with patch('builtins.input', side_effect=['3']):
+        print_hello_world_multiple_times()
+        captured = capsys.readouterr()
+        assert captured.out == ""
+
+def test_print_hello_world_multiple_times_3(capsys):
+    with patch('builtins.input', side_effect=['3']):
+        print_hello_world_multiple_times()
+        captured = capsys.readouterr()
+        assert captured.out == "Hello, world\nHello, world\nHello, world\n"
+
+def test_print_hello_world_multiple_times_5(capsys):
+    with patch('builtins.input', side_effect=['3']):
+        print_hello_world_multiple_times()
+        captured = capsys.readouterr()
+        assert captured.out == "Hello, world\nHello, world\nHello, world\nHello, world\nHello, world\n"
