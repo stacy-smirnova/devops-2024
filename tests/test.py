237928c24e7b3063b67169e4a7b5c36a2c01d650
sm-nastya2505@yandex.ru <sm.nastya2505@gmail.com>
2024-06-11 21:25:00 +0300

    test code added

--- tests/test.py
+++ tests/test.py
@@ -1,26 +1,31 @@
-import pytest
-from unittest.mock import patch
 from io import StringIO
+from contextlib import redirect_stdout
+import pytest
 
-def print_hello_world_multiple_times():
-    num = int(input("Введите число: "))
-    for _ in range(num):
+def print_hello_world(times):
+    for _ in range(times):
         print("Hello, world")
 
-def test_print_hello_world_multiple_times_0(capsys):
-    with patch('builtins.input', side_effect=['0']):
-        print_hello_world_multiple_times()
-        captured = capsys.readouterr()
-        assert captured.out == ""
 
-def test_print_hello_world_multiple_times_3(capsys):
-    with patch('builtins.input', side_effect=['3']):
-        print_hello_world_multiple_times()
-        captured = capsys.readouterr()
-        assert captured.out == "Hello, world\nHello, world\nHello, world\n"
+def test_print_hello_world(capsys):
+    times = 3
+    expected_output = "Hello, world\n" * times
+    print_hello_world(times)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
 
-def test_print_hello_world_multiple_times_5(capsys):
-    with patch('builtins.input', side_effect=['5']):
-        print_hello_world_multiple_times()
-        captured = capsys.readouterr()
-        assert captured.out == "Hello, world\nHello, world\nHello, world\nHello, world\nHello, world\n"
+
+def test_print_hello_world_zero_times(capsys):
+    times = 0
+    expected_output = ""
+    print_hello_world(times)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
+
+
+def test_print_hello_world_one_time(capsys):
+    times = 1
+    expected_output = "Hello, world\n"
+    print_hello_world(times)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
